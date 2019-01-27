527f15f9503618772f549d5f955dcb9bc8870630
Easton Gorishek <ieaston26@gmail.com>
2019-01-27 15:28:35 -0800

    javascript - append userinput to unordered list

--- src/index.js
+++ src/index.js
@@ -6,3 +6,20 @@ revealButton.addEventListener('click', function() {
   answer.classList.remove('hidden');
   revealButton.disabled = true;
 });
+
+const itemForm = document.getElementById('add-item');
+
+itemForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const item = itemForm.elements.userInput;
+
+  const liElement = document.createElement('li');
+  liElement.textContent = item.value;
+
+  const unorderedList = document.getElementById('unordered-list');
+  unorderedList.appendChild(liElement);
+
+  item.value = '';
+  item.focus();
+});
